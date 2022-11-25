4d6b4cd6f35c7439c4bbcf537e08e14a36ae452b
Paul Thomas <paul@asup.co.uk>
2022-11-25 08:54:43 +0000

    Change rollup typescript compiler

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,6 +1,6 @@
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
 import dts from 'rollup-plugin-dts';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
